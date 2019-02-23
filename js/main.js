d9c9562c7876bb267247643488ed82c3edcc0216
Evaldo Jr <evaldojunior1997@hotmail.com>
2019-02-22 22:13:11 -0300

    Ajustando responsividade e menu hamburguer

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
-$(document).ready(function() {
-      // Transition effect for navbar 
-      $(window).scroll(function() {
-        // checks if window is scrolled more than 500px, adds/removes solid class
-        if($(this).scrollTop() > 100) { 
-            $('.nav').addClass('cor');
-        } else {
-            $('.nav').removeClass('cor');
-        }
-      });
+$(document).ready(function () {
+  // Transition effect for navbar 
+  $(window).scroll(function () {
+    // checks if window is scrolled more than 500px, adds/removes solid class
+    if ($(this).scrollTop() > 100) {
+      $('.nav').addClass('cor');
+    } else {
+      $('.nav').removeClass('cor');
+    }
+  });
+});
+
+
+//navbar
+
+$(document).ready(function () {
+
+  $('.first-button').on('click', function () {
+
+    $('.animated-icon1').toggleClass('open');
+  });
+  $('.second-button').on('click', function () {
+
+    $('.animated-icon2').toggleClass('open');
+  });
+  $('.third-button').on('click', function () {
+
+    $('.animated-icon3').toggleClass('open');
+  });
 });
